8159a54eac47c1f40f294e4a89a51c001555a73e
deegles <diego@vokkal.co>
2017-08-24 22:01:36 -0700

    Add additional params to skill context.

--- src/definitions/SkillContext.ts
+++ src/definitions/SkillContext.ts
@@ -1,15 +1,39 @@
+import {Callback, Context} from "aws-lambda";
+import {AlexaRequestBody} from "./AlexaService";
+
 namespace Skill {
     export class Context {
-        constructor() {
+        constructor(request: AlexaRequestBody, event: Context, callback: Callback, _this?: any) {
+            if (_this) {
+                Object.assign(this, _this);
+            }
 
+            this.request = request;
+            this.event = event;
+            this.callback = callback;
         }
 
-        get = (key: string): string | number => {
-            return "a val";
+        request: AlexaRequestBody;
+
+        event: Context;
+
+        callback: Callback;
+
+        get(prop): any {
+            console.log(`Fetching prop ${prop}, ${prop in this ? "found" : "not found"}.`);
+            return this[prop];
         }
 
-        set = (key: string, val: string | number): boolean => {
-            return true;
+        set(prop, value): boolean {
+            console.log(`Adding prop ${prop}...`);
+
+            return this[prop] = value;
+        }
+
+        delete(prop): boolean {
+            console.log(`Deleting prop ${prop}...`);
+
+            return delete this[prop];
         }
     }
 }
