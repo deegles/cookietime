1bc9e975463fdde6040fef9803f980dd68ef06a1
deegles <diego@vokkal.co>
2017-08-24 22:00:29 -0700

    Move FrameDirectory class definition and add response model to constructor.

--- src/definitions/FrameDirectory.ts
+++ src/definitions/FrameDirectory.ts
@@ -1,8 +1,30 @@
+import {Frame} from "./Handler";
+
+export class FrameDirectory {
+    constructor() {
+
+    }
+
+    get(prop): Frame | undefined {
+        console.log(`Fetching frame ${prop}, ${prop in this ? "found" : "not found"}.`);
+        return this[prop];
+    }
+
+    set(prop, value): boolean {
+        console.log(`adding frame ${prop}...`);
+
+        if (prop in this) {
+            throw new Error(`Error, frame ${prop} already exists. All frame IDs must be unique.`);
+        }
+
+        return this[prop] = value;
+    }
+}
+
 // from https://derickbailey.com/2016/03/09/creating-a-true-singleton-in-node-js-with-es6-symbols/
 // create a unique, global symbol name
 // -----------------------------------
 
-import {FrameDirectory} from "./Handler";
 const FRAME_DIRECTORY_KEY = Symbol.for("CookieTime.namespace.FrameDirectory");
 
 // check if the global object has this symbol
@@ -13,6 +35,7 @@ let globalSymbols = Object.getOwnPropertySymbols(global);
 let hasFrameDir = (globalSymbols.indexOf(FRAME_DIRECTORY_KEY) > -1);
 
 if (!hasFrameDir) {
+    console.log("Initializing Frame directory...");
     global[FRAME_DIRECTORY_KEY] = new FrameDirectory();
 }
 
--- src/definitions/Handler.ts
+++ src/definitions/Handler.ts
@@ -2,26 +2,6 @@ import {Context} from "./SkillContext";
 let Frames = require("../definitions/FrameDirectory");
 
 namespace Handlers {
-    export class FrameDirectory {
-        constructor() {
-
-        }
-
-        get(prop): Frame | undefined {
-            console.log(`Fetching frame ${prop}, ${prop in this ? "found" : "not found"}.`);
-            return this[prop];
-        }
-
-        set(prop, value): boolean {
-            console.log(`setting frame ${prop}`);
-
-            if (prop in this) {
-                throw new Error(`Error, frame ${prop} already exists. All frame IDs must be unique.`);
-            }
-
-            return this[prop] = value;
-        }
-    }
 
     export class Frame {
         constructor(id: string, entry: ReturnsResponseContext, FrameMap: ActionMap) {
@@ -40,11 +20,10 @@ namespace Handlers {
     }
 
     export class ResponseContext {
-        constructor() {
-
+        constructor(model: ResponseModel) {
+            this.model = model;
         }
 
-        CurrentFrame: Frame;
         model: ResponseModel;
     }
 
