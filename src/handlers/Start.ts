f96dcc771b0859a365717651bb807147c29396e0
deegles <diego@vokkal.co>
2017-08-26 00:22:15 -0700

    Use import instead of require. Use attributes param.

--- src/handlers/Start.ts
+++ src/handlers/Start.ts
@@ -1,23 +1,28 @@
 import {Frame, ResponseContext, ResponseModel} from "../definitions/Handler";
-import {Context} from "../definitions/SkillContext";
-import "./Start";
+import {Attributes, RequestContext} from "../definitions/SkillContext";
 
+import * as Frames from "../definitions/FrameDirectory";
 
-let Frames = require("../definitions/FrameDirectory");
-
-let entry = (ctx: Context) => {
+let entry = (attr: Attributes, ctx: RequestContext) => {
 
     let model = new ResponseModel();
 
     model.speech = "hello";
     model.reprompt = "hello again";
 
+    attr["myprop"] = "1";
+
     return new ResponseContext(model);
 };
 
 let actionMap = {
-    "LaunchRequest": () => {
+    "LaunchRequest": (attr: Attributes) => {
+        attr["launch"] = 1;
         return Frames["Start"];
+    },
+    "AMAZON.NoIntent": (attr: Attributes) => {
+        attr["no"] = 1;
+        return Frames["InProgress"];
     }
 };
 
