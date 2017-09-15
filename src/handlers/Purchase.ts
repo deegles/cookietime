b663de11ca50e458bd31b2e0aa56dcf2995460e3
deegles <diego@vokkal.co>
2017-09-15 00:11:26 -0500

    Add purchased item to inventory.

--- src/handlers/Purchase.ts
+++ src/handlers/Purchase.ts
@@ -1,8 +1,10 @@
+import * as Big from "bignumber.js";
 import {Frame, ResponseContext, ResponseModel} from "../definitions/Handler";
 import {Attributes, RequestContext} from "../definitions/SkillContext";
 
 import * as Frames from "../definitions/FrameDirectory";
-import {Items} from "../definitions/Inventory";
+import {Items, ItemTypes, Kitchen, Purchaseable} from "../definitions/Inventory";
+import {calculateCost} from "../resources/store";
 
 let entry = (attr: Attributes, ctx: RequestContext) => {
 
@@ -11,23 +13,55 @@ let entry = (attr: Attributes, ctx: RequestContext) => {
     let ovens = ctx.slots["OVENS"];
     let kitchens = ctx.slots["KITCHENS"];
 
-    let item;
+    let itemId;
     if (ovens && ovens.resolution) {
         if (ovens.resolution.id) {
-            item = ovens.resolution.id; // TODO: fetch full name
+            itemId = ovens.resolution.id; // TODO: fetch full name
         } else {
             console.log("Invalid OVENS value: " + ovens.resolution.name);
         }
     } else if (kitchens && kitchens.resolution) {
         if (kitchens.resolution.id) {
-            item = kitchens.resolution.id; // TODO: fetch full name
+            itemId = kitchens.resolution.id; // TODO: fetch full name
         } else {
             console.log("Invalid KITCHENS value: " + ovens.resolution.name);
         }
     }
 
-    if (item) {
-        model.speech = "You bought " + item;
+    if (itemId) {
+        let allItems: Array<ItemTypes> = [].concat(attr.Inventory.Ovens, attr.Inventory.Kitchen, attr.Inventory.Assistants);
+        let item = Items.All[itemId];
+
+        function byRank(a: ItemTypes, b: ItemTypes) {
+            return Items.All[a].rank - Items.All[b].rank;
+        }
+
+        if (item.type === "Oven") {
+            let owned = allItems.filter(invItem => {
+                return Items.All[invItem].type === "Oven";
+            });
+
+
+            console.log("owned: " + owned);
+            let cost = calculateCost(item, owned.length);
+            console.log("cookies: " + attr.CookieCounter);
+            console.log("cost: " + cost);
+
+            attr.Inventory.Ovens.push(itemId);
+            attr.Inventory.Ovens.sort(byRank);
+
+            let kitchen = Items.All[attr.Inventory.Kitchen] as Kitchen;
+            attr.Inventory.Ovens = attr.Inventory.Ovens.slice(0, kitchen.OvenLimit);
+
+            attr.CookieCounter = attr.CookieCounter.minus(cost);
+
+            if (attr.CookieCounter.lessThan(0)) {
+                attr.CookieCounter = new Big(0);
+                console.log("ERROR: cost was greater than available cookies");
+            }
+        }
+
+        model.speech = "You now have " + attr.Inventory.Ovens.join(", ");
     } else {
         model.speech = "You can't purchase that.";
     }
