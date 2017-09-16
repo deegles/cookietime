0bf12ebce297b8aa1817eefe9e8338cacfd45360
deegles <diego@vokkal.co>
2017-09-16 14:42:04 -0500

    Add helper for Microsoft LUIS.

---
+++ src/resources/LUIS.ts
@@ -0,0 +1,36 @@
+import * as request from "request";
+import {LUISServiceResponse} from "../definitions/LUISService";
+
+let luisSubKey = process.env["luisSubKey"];
+let luisAppId = process.env["luisAppId"];
+
+let requestCache: { [key: string]: LUISServiceResponse } = {}; // TODO: extend with a dynamodb layer
+
+/**
+ * Performs NLU using LUIS.
+ */
+export const query = function (query: string): Promise<LUISServiceResponse> {
+    return new Promise((resolve, reject) => {
+        try {
+            if (query in requestCache) {
+                return resolve(requestCache[query]);
+            }
+
+            request.get({
+                url: `https://eastus2.api.cognitive.microsoft.com/luis/v2.0/apps/${luisAppId}?subscription-key=${luisSubKey}&timezoneOffset=0&verbose=false&q=${query}`,
+            }, function (error, response, body) {
+                console.log("Response: " + body);
+                if (response.statusCode === 200) {
+                    let luisResponse = JSON.parse(body) as LUISServiceResponse;
+                    requestCache[query] = luisResponse;
+                    resolve(luisResponse);
+                } else {
+                    reject(error);
+                }
+            });
+        } catch (e) {
+            console.log("EXCEPTION " + e);
+            reject(e);
+        }
+    });
+};
