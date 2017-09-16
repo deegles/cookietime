2dcc9ce489f89898c97f1b1cdaea103e42cac0e5
deegles <diego@vokkal.co>
2017-09-16 18:32:15 -0500

    Refactor and extend Bot service definition.

--- src/definitions/BotFrameworkService.ts
+++ src/definitions/BotFrameworkService.ts
@@ -1,4 +1,5 @@
 /***
+ * https://docs.microsoft.com/en-us/bot-framework/rest-api/bot-framework-rest-connector-api-reference
  * @example
  {
   "type": "message",
@@ -38,10 +39,17 @@
 
 import {RequestBody} from "./Common";
 
-export type BotFrameworkRequestType = "message";
+export type BotFrameworkActivityType =
+    "contactRelationUpdate"
+    | "conversationUpdate"
+    | "deleteUserData"
+    | "message"
+    | "ping"
+    | "typing"
+    | "endOfConversation";
 
-export const BotFrameworkRequestType = {
-    message: "message" as BotFrameworkRequestType
+export const BotFrameworkActivityType = {
+    message: "message" as BotFrameworkActivityType
 };
 
 export type BotFrameworkChannelType = "cortana";
@@ -63,26 +71,64 @@ export const BotFrameworkEntity = {
     "DeviceInfo": "DeviceInfo" as BotFrameworkEntity
 };
 
-export interface BotFrameWorkRequestBody extends RequestBody {
-    type: BotFrameworkRequestType;
-    id: string;
-    timestamp: string;
-    serviceUrl: string;
-    channelId: BotFrameworkChannelType;
-    from: {
-        id: string;
-    };
-    conversation: {
-        id: string;
-    };
-    recipient: {
-        id: string;
-    };
-    locale: BotFrameworkLocale;
-    text: string;
-    entities: Array<Entity>;
+export interface BotFrameworkActivity extends RequestBody {
+    action: string;
+    attachments: Array<string>; // TODO: attachment object
+    attachmentLayout: string;
     channelData: ChannelData;
+    channelId: BotFrameworkChannelType;
+    conversation: ConversationAccount;
+    entities: Array<Entity>;
+    from: ChannelAccount;
+    historyDisclosed: boolean;
+    id: string;
+    inputHint: string;
+    locale: BotFrameworkLocale;
+    localTimestamp: string;
+    membersAdded: Array<ChannelAccount>;
+    membersRemoved: Array<ChannelAccount>;
+    recipient: ChannelAccount;
+    relatesTo: string; // TODO: ConversationReference
+    replyToId: string;
+    serviceUrl: string;
+    speak: string;
+    suggestedActions: any; // TODO: SuggestedActions
+    summary: string;
+    text: string;
+    textFormat: string;
+    timestamp: string;
+    topicName: string;
+    type: BotFrameworkActivityType;
+}
 
+/**
+ * Defines a bot or user account on the channel.
+ */
+export interface ChannelAccount {
+    /**
+     * ID that uniquely identifies the bot or user on the channel. */
+    id: string;
+    /**
+     * Name of the bot or user. */
+    name: string;
+}
+
+/**
+ * Defines a conversation in a channel.
+ */
+export interface ConversationAccount {
+    /**
+     * The ID that identifies the conversation. The ID is unique per channel. If the channel starts
+     * the conversion, it sets this ID; otherwise, the bot sets this property to the ID that it gets
+     * back in the response when it starts the conversation. */
+    id: string;
+    /**
+     * Flag to indicate whether or not this is a group conversation. Set to true if this is a group
+     * conversation; otherwise, false. The default is false. */
+    isGroup: boolean;
+    /**
+     * A display name that can be used to identify the conversation. */
+    name: string;
 }
 
 export interface Entity {
