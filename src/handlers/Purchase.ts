8b05a1f59d8eb60c8803d31ecc87b82e51916cf7
deegles <diego@vokkal.co>
2017-09-24 12:48:01 -0400

    Fix logic for detecting duplicate items and preventing purchasing lower tier items unless inventory has open slots.

--- src/handlers/Purchase.ts
+++ src/handlers/Purchase.ts
@@ -4,7 +4,7 @@ import {Attributes, RequestContext} from "../definitions/SkillContext";
 
 import * as Frames from "../definitions/FrameDirectory";
 import {Items, ItemTypes, Kitchen, Purchaseable} from "../definitions/Inventory";
-import {calculateCost} from "../resources/store";
+import {calculateCost, getPurchaseableItems} from "../resources/store";
 
 let entry = (attr: Attributes, ctx: RequestContext) => {
 
@@ -13,56 +13,84 @@ let entry = (attr: Attributes, ctx: RequestContext) => {
     let ovens = ctx.slots["OVENS"];
     let kitchens = ctx.slots["KITCHENS"];
 
+    console.log("Context: " + JSON.stringify(ctx));
+    console.log("Inventory: " + JSON.stringify(attr.Inventory));
+
     let itemId;
+    let unresolvedItem = false;
     if (ovens && ovens.resolution) {
         if (ovens.resolution.id) {
             itemId = ovens.resolution.id; // TODO: fetch full name
         } else {
+            unresolvedItem = true;
             console.log("Invalid OVENS value: " + ovens.resolution.name);
         }
     } else if (kitchens && kitchens.resolution) {
         if (kitchens.resolution.id) {
             itemId = kitchens.resolution.id; // TODO: fetch full name
         } else {
+            unresolvedItem = true;
             console.log("Invalid KITCHENS value: " + ovens.resolution.name);
         }
     }
 
     if (itemId) {
         let allItems: Array<ItemTypes> = [].concat(attr.Inventory.Ovens, attr.Inventory.Kitchen, attr.Inventory.Assistants);
-        let item = Items.All[itemId];
+        let candidate = Items.All[itemId];
 
         function byRank(a: ItemTypes, b: ItemTypes) {
             return Items.All[b].rank - Items.All[a].rank;
         }
 
-        if (item.type === "Oven") {
-            let owned = allItems.filter(invItem => {
-                return Items.All[invItem].type === "Oven";
-            });
+        let cost = calculateCost(candidate, attr.Inventory);
+        console.log("cookies: " + attr.CookieCounter);
+        console.log("cost: " + cost);
 
-            console.log("owned: " + owned);
-            let cost = calculateCost(item, owned.length);
-            console.log("cookies: " + attr.CookieCounter);
-            console.log("cost: " + cost);
+        let kitchen = Items.All[attr.Inventory.Kitchen] as Kitchen;
 
+        let ovensBelowItemRank = attr.Inventory.Ovens.some(invItem => {
+            let t = Items.All[invItem];
+            console.log("Item: " + candidate.id + " : " + candidate.rank);
+            console.log("inv: " + t.id + " : " + t.rank);
+            return t.type === candidate.type && t.rank < candidate.rank;
+        });
+
+        let assistantsBelowItemRank = attr.Inventory.Assistants.some(invItem => {
+            let t = Items.All[invItem];
+            return t.type === candidate.type && t.rank < candidate.rank;
+        });
+
+        let ovenSpaceFull = !ovensBelowItemRank && candidate.type === "Oven" && attr.Inventory.Ovens.length >= kitchen.OvenLimit;
+        let assistantSpaceFull = !assistantsBelowItemRank && candidate.type === "Assistant" && attr.Inventory.Assistants.length >= kitchen.AssistantLimit;
+
+        if (ovensBelowItemRank && assistantsBelowItemRank) {
+            model.speech = "You already have better items.";
+        } else if (ovenSpaceFull || assistantSpaceFull) {
+            model.speech = "You don't have room for that.";
+        } else if (attr.CookieCounter.lessThan(cost)) {
+            console.log("ERROR: oven cost was greater than available cookies");
+            model.speech = "You don't have enough cookies to purchase that. ";
+        } else if (candidate.type === "Oven") {
             attr.Inventory.Ovens.push(itemId);
             attr.Inventory.Ovens.sort(byRank);
 
-            let kitchen = Items.All[attr.Inventory.Kitchen] as Kitchen;
             attr.Inventory.Ovens = attr.Inventory.Ovens.slice(0, kitchen.OvenLimit);
 
             attr.CookieCounter = attr.CookieCounter.minus(cost);
+            attr.Upgrades = getPurchaseableItems(attr.CookieCounter, attr.Inventory);
+            model.speech = "You now have " + attr.Inventory.Ovens.join(", ");
 
-            if (attr.CookieCounter.lessThan(0)) {
-                attr.CookieCounter = new Big(0);
-                console.log("ERROR: cost was greater than available cookies");
-            }
+        } else if (candidate.type === "Kitchen") {
+
+            attr.Inventory.Kitchen = itemId;
+
+            attr.CookieCounter = attr.CookieCounter.minus(cost);
+            attr.Upgrades = getPurchaseableItems(attr.CookieCounter, attr.Inventory);
+            model.speech = "You now have " + attr.Inventory.Ovens.join(", ");
         }
 
-        model.speech = "You now have " + attr.Inventory.Ovens.join(", ");
     } else {
-        model.speech = "You can't purchase that.";
+        model.speech = unresolvedItem ? "You can't purchase that." : "What would you like to purchase?";
     }
 
     model.reprompt = "What else would you like to purchase?";
