0f8388c5d756e9bf6c3a551f6efa70465b26a4b8
deegles <diego@vokkal.co>
2017-09-24 12:45:28 -0400

    Add more oven types and balance prices.

--- src/definitions/Inventory.ts
+++ src/definitions/Inventory.ts
@@ -29,7 +29,15 @@ export interface Assistant extends Purchaseable {
 }
 
 export type ItemCategories = "Oven" | "Assistant" | "Kitchen";
-export type OvenTypes = "EasyBake" | "HomeOven";
+export type OvenTypes =
+    "EasyBake"
+    | "Fancy"
+    | "Toaster"
+    | "HomeOven"
+    | "Professional"
+    | "Industrial"
+    | "Continuous"
+    | "UpgradedContinuous";
 export type AssistantTypes = "PartTime";
 export type KitchenTypes = "Hobby";
 export type ItemTypes = OvenTypes | AssistantTypes | KitchenTypes;
@@ -40,22 +48,76 @@ export class Items {
          * Ovens
          */
         EasyBake: {
-            baseCost: new Big(25),
-            multiplier: 1.03,
+            baseCost: new Big(5),
+            multiplier: 1.07,
             hourlyRate: 2,
             capacity: 1,
             type: "Oven",
             id: "EasyBake",
             rank: 1
         } as Oven,
+        Toaster: {
+            baseCost: new Big(10),
+            multiplier: 1.07,
+            hourlyRate: 4,
+            capacity: 2,
+            type: "Oven",
+            id: "Toaster",
+            rank: 2
+        } as Oven,
         HomeOven: {
-            baseCost: new Big(100),
+            baseCost: new Big(20),
             multiplier: 1.1,
             hourlyRate: 8,
             capacity: 4,
             type: "Oven",
             id: "HomeOven",
-            rank: 2
+            rank: 3
+        } as Oven,
+        Fancy: {
+            baseCost: new Big(50),
+            multiplier: 1.1,
+            hourlyRate: 16,
+            capacity: 8,
+            type: "Oven",
+            id: "Fancy",
+            rank: 4
+        } as Oven,
+        Professional: {
+            baseCost: new Big(160),
+            multiplier: 1.2,
+            hourlyRate: 32,
+            capacity: 16,
+            type: "Oven",
+            id: "Professional",
+            rank: 5
+        } as Oven,
+        Industrial: {
+            baseCost: new Big(500),
+            multiplier: 1.3,
+            hourlyRate: 64,
+            capacity: 32,
+            type: "Oven",
+            id: "Industrial",
+            rank: 6
+        } as Oven,
+        Continuous: {
+            baseCost: new Big(1000),
+            multiplier: 1.5,
+            hourlyRate: 128,
+            capacity: 64,
+            type: "Oven",
+            id: "Continuous",
+            rank: 7
+        } as Oven,
+        UpgradedContinuous: {
+            baseCost: new Big(2500),
+            multiplier: 1.7,
+            hourlyRate: 256,
+            capacity: 128,
+            type: "Oven",
+            id: "UpgradedContinuous",
+            rank: 8
         } as Oven,
         /**
          * Assistants
