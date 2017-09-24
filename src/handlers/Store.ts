ba3453ef8390848d60e7a7ae1a08b673ae9a14d4
deegles <diego@vokkal.co>
2017-09-24 12:50:30 -0400

    Fix handling of dialog around available upgrades and next upgrade availability.

--- src/handlers/Store.ts
+++ src/handlers/Store.ts
@@ -3,18 +3,32 @@ import {Attributes, RequestContext} from "../definitions/SkillContext";
 
 import * as Frames from "../definitions/FrameDirectory";
 import {Items} from "../definitions/Inventory";
+import {Humanize} from "../resources/humanize";
+import {getNextUpgradeCost} from "../resources/store";
 
 let entry = (attr: Attributes, ctx: RequestContext) => {
 
     let model = new ResponseModel();
+    attr.NextUpgrade = getNextUpgradeCost(attr.Inventory);
 
-    model.speech = "You can purchase: " + attr.Upgrades.map(itemId => {
-            let item = Items.All[itemId];
+    if (attr.Upgrades.length > 0) {
+        model.speech = "You can purchase: " + attr.Upgrades.map(itemId => {
+                let item = Items.All[itemId];
 
-            return item.id;
-        }).join(", ") + ". What would you like to purchase?";
+                return item.id;
+            }).join(", ") + ". What would you like to purchase?";
 
-    model.reprompt = "What would you like to purchase?";
+        model.reprompt = "What would you like to purchase?";
+    } else {
+        if (attr.NextUpgrade.greaterThan(0)) {
+            model.speech = "There are no items available for purchase right now. The next upgrade is available at " + Humanize(attr.NextUpgrade, 4) + " cookies.";
+        } else {
+            model.speech = "There are no items available for purchase right now. Bake more cookies!";
+
+        }
+
+        model.reprompt = "You need more cookies.";
+    }
 
     return new ResponseContext(model);
 };
@@ -29,6 +43,9 @@ let actionMap = {
     "EatCookieIntent": (attr: Attributes) => {
         return Frames["Eat"];
     },
+    "CookieIntent": (attr: Attributes) => {
+        return Frames["Cookie"];
+    }
 };
 
 let unhandled = () => {
