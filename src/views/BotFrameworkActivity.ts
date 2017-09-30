31e7d5deb4ee8a075c65e291a5a998c2322c1d13
deegles <diego@vokkal.co>
2017-09-30 11:32:00 -0400

    Render hero cards for Cortana responses.

--- src/views/BotFrameworkActivity.ts
+++ src/views/BotFrameworkActivity.ts
@@ -1,20 +1,91 @@
-import {BotFrameworkActivity} from "../definitions/BotFrameworkService";
+import {BotFrameworkActivity, BotFrameworkAttachment} from "../definitions/BotFrameworkService";
 import {ResponseModel} from "../definitions/Handler";
+import {Items, Purchaseable} from "../definitions/Inventory";
 import {View} from "../definitions/Views";
 
 new View("BotFrameworkActivity", (model: ResponseModel, activity: BotFrameworkActivity) => {
 
+    let CookieAction = {
+        text: "Cookie",
+        title: "Cookie Time",
+        type: "postBack",
+        value: "bake more cookies",
+    };
+
+    let UpgradesAction = {
+        text: "Upgrades",
+        title: "Check Upgrades",
+        type: "postBack",
+        value: "check for upgrades",
+    };
+
+    let EatAction = {
+        text: "eat",
+        title: "Eat a Cookie!",
+        type: "postBack",
+        value: "eat a cookie",
+    };
+
+    let HelpAction = {
+        text: "Help",
+        title: "Help",
+        type: "postBack",
+        value: "help",
+    };
+
+    let card = {
+        title: "Cookie Time",
+        text: "Tap this card to bake more cookies!",
+        tap: CookieAction,
+        buttons: [CookieAction]
+    };
+
+    if (model.cookieCount) {
+        card.title = "Cookie Count: " + model.cookieCount.toFormat(0);
+    }
+
+    if (model.upgrades.length > 0) {
+
+        model.upgrades.forEach(upgrade => {
+            let item: Purchaseable = Items.All[upgrade];
+            console.log("upgrade: " + upgrade);
+            console.log("item:" + JSON.stringify(item));
+            card.buttons.push(
+                {
+                    text: "Upgrade",
+                    title: "Buy: " + item.id,
+                    type: "postBack",
+                    value: item.description,
+                }
+            );
+        });
+    } else {
+        card.buttons.push(UpgradesAction);
+    }
+
+    card.buttons.push(EatAction, HelpAction);
+
+    let Attachment = {
+        contentType: "application/vnd.microsoft.card.hero",
+        content: card,
+        name: "Cookie Card",
+    } as BotFrameworkAttachment;
+
     let response = {
-        "type": "message",
-        "from": activity.recipient,
-        "conversation": activity.conversation,
-        "locale": activity.locale,
-        "recipient": activity.from,
-        "text":  model.speech,
-        "speak": model.speech,
-        "inputHint": "expectingInput",
-        "replyToId": activity.id
+        type: "message",
+        from: activity.recipient,
+        conversation: activity.conversation,
+        locale: activity.locale,
+        recipient: activity.from,
+        speak: model.speech,
+        inputHint: "expectingInput",
+        replyToId: activity.id,
+        attachments: [Attachment],
     } as BotFrameworkActivity;
+
+    if (activity.channelId === "webchat" && model.upgrades.length < 1) {
+        response.text = model.speech;
+    }
 
     return response;
 });
